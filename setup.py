ef1ba9c6ccd848f4fa3f929b55af7d3627755807
samuelstanton <ss13641@nyu.edu>
2021-05-06 15:27:01 -0400

    update requirements

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
+from os import path
 
 from setuptools import find_packages, setup
 
 AUTHOR = "S. Stanton"
 NAME = "upcycle"
 PACKAGES = find_packages()
+
+REQUIREMENTS_FILE = "requirements.txt"
+REQUIREMENTS_PATH = path.join(path.abspath(__file__), REQUIREMENTS_FILE)
+with open(REQUIREMENTS_FILE) as f:
+    requirements = f.read().splitlines()
 
 setup(
     name=NAME,
@@ -13,5 +19,6 @@ setup(
     author=AUTHOR,
     author_email='ss13641@nyu.edu',
     url='https://github.com/samuelstanton/upcycle',
+    install_requires=requirements,
     packages=PACKAGES,
 )
