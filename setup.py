2e021f2aae63df5ee12715c572624123a76ea7ac
samuelstanton <ss13641@nyu.edu>
2021-05-06 15:20:41 -0400

    update setup script

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import find_packages, setup
+
+AUTHOR = "S. Stanton"
+NAME = "upcycle"
+PACKAGES = find_packages()
 
 setup(
-    name='upcycle',
-    version='0.1',
+    name=NAME,
+    version='0.0.1',
     description='Reusable code snippets',
-    author='Samuel Stanton',
+    author=AUTHOR,
     author_email='ss13641@nyu.edu',
     url='https://github.com/samuelstanton/upcycle',
+    packages=PACKAGES,
 )
